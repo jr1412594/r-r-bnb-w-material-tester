76832dc462ade88d18b60f89a8f54c33c5f4d3eb
jr1412594 <jr.medina1412@gmail.com>
2021-02-02 15:40:38 -0700

    revert

--- src/Components/CaracterCard.js
+++ src/Components/CaracterCard.js
@@ -5,7 +5,7 @@ export default function CaracterCard({character}) {
     return (
         <div className='characterCard'>
             <h2 className ='characterName'>{character.name}</h2>
-            <img className='characeterImage' src={character.image} alt={character.name}/>
+            <img className='characterImage' src={character.image} alt={character.name}/>
             <div className='characterInfoContainer'>
                 <h3 className='characterLocation'>{character.location.name}</h3>
                 <h3 className='characterSpecies'>{character.species}</h3>
